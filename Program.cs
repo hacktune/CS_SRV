fc3541338ca8bd766a5f4d2bf31bf8619de8fc72
hacktune <hacktunw@gmail.com>
2012-11-30 21:15:59 +0900

    ss
    ss

---
+++ Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class MyTcpListener
+{
+    public static void Main()
+    {
+        TcpListener server = null;
+        try
+        {
+            Int32 port = 8080;
+            IPAddress localAddr = IPAddress.Parse("127.0.0.1");
+            server = new TcpListener(localAddr, port);
+            server.Start();
+            Byte[] bytes = new Byte[1024];
+            String data = null;
+            while (true)
+            {
+                Console.Write("Waiting for a connection... ");
+                TcpClient client = server.AcceptTcpClient();
+                Console.WriteLine("Connected!");
+                data = null;
+                NetworkStream stream = client.GetStream();
+                int i;
+             
+                while ((i = stream.Read(bytes, 0, bytes.Length)) != 0)
+                {
+                    FileStream fs = new FileStream("txt.txt", FileMode.Append, FileAccess.Write);
+                    StreamWriter sw = new StreamWriter(fs, System.Text.Encoding.UTF8);
+                    // Translate data bytes to a ASCII string.
+                    data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                    Console.WriteLine("Received: {0}", data);
+                    sw.WriteLine(data);
+
+                    data = data.ToUpper();
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+                    stream.Write(msg, 0, msg.Length);
+                    Console.WriteLine("Sent: {0}", data);
+                    
+                    sw.Flush();
+                    sw.Close();
+                    fs.Close();
+                }
+
+                client.Close();
+            }
+        }
+        catch (SocketException e)
+        {
+            Console.WriteLine("SocketException: {0}", e);
+        }
+        finally
+        {
+            // Stop listening for new clients.
+            server.Stop();
+        }
+
+
+        Console.WriteLine("\nHit enter to continue...");
+        Console.Read();
+    }
+}
